5bb806171170de2df1c0c6bc5cf21175cd64f04b
Rononoa13 <sumitluitel42@gmail.com>
2022-02-15 23:07:15 +0545

    Attempt 7

--- 2022/candy.py
+++ 2022/candy.py
@@ -1,15 +1,15 @@
-total_bags = 7
-num_kids = 3
-
 def cases(case_num):
 
+    (total_bags, num_kids) = map(int, input().split())
+    # print("total bags ->", total_bags)
+    # print("total kids ->", num_kids)
     total_candy = 0
-    for i in range(total_bags+1):
+    for i in range(total_bags + 1):
         total_candy += i
     # print("total candy ->", total_candy)
     candy_remaining = total_candy % num_kids
     # print("Candy remains ->", candy_remaining)
-    print('Case #%d: %d.', case_num, candy_remaining)
+    print(f'Case #{case_num}: {candy_remaining}')
 
 # Iterater through each case
 num_cases = int(input())
